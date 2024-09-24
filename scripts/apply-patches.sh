abe699a544b80ca0eb9b8084d8ea4f8f0ba8b2a6
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-09-24 11:22:42 +0200

    Updates.

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -3,8 +3,6 @@
 # current working directory
 WORK_DIR=$(pwd)
 
-# time offset between UTC and the local time
-OFFSET_PARIS=$(TZ=Europe/Paris date +%z)
 OFFSET_MELBOURNE=$(TZ=Australia/Melbourne date +%z)
 
 cd "$1" || exit 1
@@ -12,10 +10,6 @@ cd "$1" || exit 1
 git restore .
 
 ########################################################################################################################
-
-if [ "$OFFSET_PARIS" == "+0200" ]; then
-  git apply "$WORK_DIR/patches/compliance-level-3/0068-feel-equality/0068-feel-equality.patch"
-fi
 
 git apply "$WORK_DIR/patches/compliance-level-3/0089-nested-inputdata-imports/0089-nested-inputdata-imports.patch"
 
@@ -34,6 +28,9 @@ git apply "$WORK_DIR/patches/compliance-level-3/1143-feel-round-half-up-function
 git apply "$WORK_DIR/patches/compliance-level-3/1144-feel-round-half-down-function/1144-feel-round-half-down-function.patch"
 
 ########################################################################################################################
+
+# remove non compliant tests from the directory tree
+rm -rf TestCases/non-compliant
 
 git status -s
 
